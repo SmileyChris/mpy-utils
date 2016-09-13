ef3cbcee3bbda4d438db8d21ebd4fe5d4cd2b31d
Chris Beaven <smileychris@gmail.com>
2016-09-13 13:50:54 +1200

    Formatting changes, make code run in a function

--- mpy_utils/upload.py
+++ mpy_utils/upload.py
@@ -1,39 +1,55 @@
 #!/usr/bin/env python
-
 import serial
 import time
 import argparse
 
 parser = argparse.ArgumentParser(
-    description="upload files to a device using only the REPL"
+    description="Upload files to a device using only the REPL"
 )
-parser.add_argument('--port', default='/dev/ttyUSB0', help='serial port device')
-parser.add_argument('--baud', default=115200, type=int, help='port speed in baud')
-parser.add_argument('--delay', default=100.0, type=float, help='delay between lines (ms)')
-parser.add_argument('--interrupt', action='store_true', help='send soft interrupt (control-C) before upload')
-parser.add_argument('--reset', action='store_true', help='send soft reset (control-D) after upload')
-parser.add_argument('files', nargs='*', type=argparse.FileType('rb'))
-args = parser.parse_args()
+parser.add_argument(
+    '--port', default='/dev/ttyUSB0', help='serial port device')
+parser.add_argument(
+    '--baud', default=115200, type=int, help='port speed in baud')
+parser.add_argument(
+    '--delay', default=100.0, type=float, help='delay between lines (ms)')
+parser.add_argument(
+    '--interrupt', action='store_true',
+    help='send soft interrupt (control-C) before upload')
+parser.add_argument(
+    '--reset', action='store_true',
+    help='send soft reset (control-D) after upload')
+parser.add_argument(
+    'files', nargs='*', type=argparse.FileType('rb'))
 
-port = serial.Serial(args.port, args.baud)
 
-if args.interrupt:
-  port.write('\x03')
+def main():
+    args = parser.parse_args()
+    if not args.files:
+        parser.print_help()
+        return
+    port = serial.Serial(args.port, args.baud)
 
-for fh in args.files:
+    if args.interrupt:
+        port.write('\x03')
 
-  port.write('_fh = open(%s, "w")\r' % repr(fh.name))
+    for fh in args.files:
+        port.write('_fh = open(%r, "w")\r' % fh.name)
 
-  while True:
-    s = fh.read(50)
-    if len(s) == 0: break
-    port.write("_fh.write(%s)\r" % repr(s))
-    time.sleep(args.delay/1000.0)
+        while True:
+            s = fh.read(50)
+            if not s:
+                break
+            port.write("_fh.write(%r)\r" % s)
+            time.sleep(args.delay/1000.0)
 
-  port.write('_fh.close()\r')
-  fh.close()
+        port.write('_fh.close()\r')
+        fh.close()
 
-if args.reset:
-  port.write('\x04')
+    if args.reset:
+        port.write('\x04')
 
-port.close()
+    port.close()
+
+
+if __name__ == '__main__':
+    main()
