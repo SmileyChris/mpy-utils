770fd4647492ec3b7383b2fc7d702231527399af
Chris Beaven <smileychris@gmail.com>
2016-09-13 16:55:41 +1200

    Separate out the comms to a class, new --main argument

--- mpy_utils/upload.py
+++ mpy_utils/upload.py
@@ -19,36 +19,71 @@ parser.add_argument(
     '--reset', action='store_true',
     help='send soft reset (control-D) after upload')
 parser.add_argument(
+    '--main', type=argparse.FileType('rb'),
+    help='save file as main.py and reset')
+parser.add_argument(
     'files', nargs='*', type=argparse.FileType('rb'))
+
+
+class Comms(object):
+
+    def __init__(self, args, interrupt=None):
+        self.args = args
+        if interrupt is None:
+            self.interrupt = args.interrupt
+        else:
+            self.interrupt = interrupt
+
+    @property
+    def port(self):
+        if not hasattr(self, '_port'):
+            self._port = serial.Serial(self.args.port, self.args.baud)
+            if self.interrupt:
+                self._port.write('\x03')
+        return self._port
+
+    def write(self, line, ending='\r'):
+        self.port.write(line)
+        if ending:
+            self.port.write(ending)
+
+    def close(self):
+        if hasattr(self, '_port'):
+            self._port.close()
+            del self._port
 
 
 def main():
     args = parser.parse_args()
-    if not args.files:
+    comms = Comms(args, interrupt=args.main or args.interrupt)
+
+    files = [(fh.name, fh) for fh in args.files]
+    if args.main:
+        files.append(('main.py', args.main))
+    if not files:
         parser.print_help()
         return
-    port = serial.Serial(args.port, args.baud)
 
-    if args.interrupt:
-        port.write('\x03')
-
-    for fh in args.files:
-        port.write('_fh = open(%r, "w")\r' % fh.name)
+    for name, fh in files:
+        comms.write('_fh = open(%r, "w")' % name)
+        comms.write('_fhw = lambda text: _fh.write(text) and None')
 
         while True:
-            s = fh.read(50)
+            s = fh.read(60)
             if not s:
                 break
-            port.write("_fh.write(%r)\r" % s)
+            comms.write("_fhw(%r)" % s)
             time.sleep(args.delay/1000.0)
 
-        port.write('_fh.close()\r')
+        comms.write('_fh.close()')
         fh.close()
 
-    if args.reset:
-        port.write('\x04')
+    if args.reset or args.main:
+        comms.write('\x04', ending=None)
+    else:
+        comms.write('del _fh')
 
-    port.close()
+    comms.close()
 
 
 if __name__ == '__main__':
