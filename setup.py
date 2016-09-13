928bf4c74ca78cbe1ab208435035c6e63d343203
Chris Beaven <smileychris@gmail.com>
2016-09-13 17:03:35 +1200

    Add the package to setup.py, duh

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mpy-tools',
     version='0.0.1',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'mpy-upload=mpy_utils.upload:main',
